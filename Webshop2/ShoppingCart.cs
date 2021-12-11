0b767d27f7ab54e3f047a862b52eddc2734b4bf6
Philipp Buchner <S2010438044@fhooe.at>
2021-12-11 11:07:05 +0100

    warehouse and shoppingcar class added

---
+++ Webshop2/ShoppingCart.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Webshop
+{
+    class ShoppingCart
+
+    {
+
+        #region  methods
+        public addToCart string ()
+        {
+           
+        }
+        #endregion
+    }
+}
