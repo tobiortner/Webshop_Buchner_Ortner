7e9e265365d79b322e5359ab0003023ea63b271f
tobiortner <tobias.ortner@students.fh-wels.at>
2021-12-11 13:47:13 +0100

    produkte in array mit methode(wirdt nu a exception deswegen auskommentiert) findest du vielleicht den fehler?

--- Webshop2/Program.cs
+++ Webshop2/Program.cs
@@ -13,6 +13,7 @@ namespace Webshop2
             //Anzahl der User erfragen
             int sizeUserArray = InsertUserArraySize();
             User[] personArray = new User[sizeUserArray];
+
             for (int i = 0; i < sizeUserArray; i++)
             {
                 personArray[i] = new User("firstname", "surname", DateTime.Today);
@@ -21,8 +22,30 @@ namespace Webshop2
                 personArray[i].FirstName = InsertFirstName("firstname");
                 personArray[i].Surname = InsertSurName("surname");
                 personArray[i].Birthdate = InsertBirthdate();
-
             }
+            Console.Clear();
+
+            int sizeArray = 7;
+            Article[] articleArray = new Article[sizeArray];
+
+            #region fill articleArray
+            articleArray[0] = new Article("Kaffee", 5, "Lebensmittel", 5.90, 12340);
+            articleArray[1] = new Article("Milch", 25, "Lebensmittel", 1.90, 12341);
+            articleArray[2] = new Article("Teddybär", 4, "Spielzeug", 24.90, 12342);
+            articleArray[3] = new Article("Spielzeugauto", 2, "Spielzeug", 34.90, 12343);
+            articleArray[4] = new Article("Waschmaschine", 15, "Haushaltsgeräte", 699.90, 12344);
+            articleArray[5] = new Article("Backrohr", 1, "Haushaltsgerät", 399.90, 12345);
+            articleArray[6] = new Article("Fernseher", 8, "Elektrowaren", 1599.90, 12346);
+            articleArray[7] = new Article("Laptop", 5, "Elektrowaren", 699.90, 12347);
+            #endregion
+            
+            //ScreenArticle(articleArray);
+
+            Console.ReadLine();
+
+
+
+
         }
         public static int InsertUserArraySize()
         {
@@ -105,6 +128,21 @@ namespace Webshop2
             } while (checkinput == false);
 
             return Birthdate;
+        }
+        public static void ScreenArticle(Article[] articleArray)
+        {
+            for (int i = 0; i <= 7; i++)
+            {
+                Console.WriteLine(i+")");
+                Console.Write(articleArray[i].Articledescription);
+                Console.Write(articleArray[i].Availability);
+                Console.Write(articleArray[i].Category);
+                Console.Write(articleArray[i].Price);
+                Console.Write(articleArray[i].Articlenumber);
+                
+                Console.WriteLine();
+
+            }
         }
     }
 }
